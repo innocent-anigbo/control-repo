2e8cc649ea3b910fac00c58a27e3bc69ecbb9707
root <root@puppet.mykezy.com>
2017-11-14 18:02:03 +0000

    coppied from main manifest

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,32 +1,12 @@
-## site.pp ##
 
-# This file (/etc/puppetlabs/puppet/manifests/site.pp) is the main entry point
-# used when an agent connects to a master and asks for an updated configuration.
-#
-# Global objects like filebuckets and resource defaults should go in this file,
-# as should the default node definition. (The default node can be omitted
-# if you use the console and don't define any other nodes in site.pp. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.)
+Package {
+      allow_virtual => true,
+    }
 
-## Active Configurations ##
-
-# Disable filebucket by default for all File resources:
-#https://docs.puppet.com/pe/2015.3/release_notes.html#filebucket-resource-no-longer-created-by-default
-File { backup => false }
-
-# DEFAULT NODE
-# Node definitions in this file are merged with node data from the console. See
-# http://docs.puppetlabs.com/guides/language_guide.html#nodes for more on
-# node definitions.
-
-# The default node definition matches any node lacking a more specific node
-# definition. If there are no other nodes in this file, classes declared here
-# will be included in every node's catalog, *in addition* to any classes
-# specified in the console for that node.
-
-node default {
-  # This is where you can declare classes for all nodes.
-  # Example:
-  #   class { 'my_class': }
+file {'/home/ec2-user/.ssh/authorized_keys':                                            # resource type file and filename
+  ensure  => present,                                               # make sure it exists
+  content => "ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAoY9fj9zwcZMSXF18WtukGqZsQTZt8qYytO43YaQG6Ubm45HOXs4s76YXIMdus1TWna5Xha/QfbAWUuD80I33nxyzd91IzqpLke8R+EGe9NQBM+Hz0fRzt17WPuBWUSrgS8ibURYgP67tBfS5GoGlFzC2+SuujL4eCsRmMHgdn5qvb/SF8LPsRoTwiqdlHkGa2FOAEq/sciYdEQoHvtAKSNYgSTkJWORpkEZ82xuOm9q68buZ+SwWzNVLChYEB5fBBQW7nT9Qdd2Uv0uwRitmHjIcWsHnueP9dVonYHH8kdIA4Zfv7Uipc5OzMO5B04ahxFm2m0BHXioCV9hci+CE2Q== innocent.anigbo@wds.co",  #innocent keys
 }
+
+
+node default {}
